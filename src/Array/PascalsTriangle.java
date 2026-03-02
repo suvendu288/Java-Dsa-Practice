50d7402f73a9d57814e93fdb4a8d41f30996d329
suvendu288 <suvendupal288@gmail.com>
2026-03-02 23:19:51 +0530

    Implement Pascal's Triangle generator

---
+++ src/Array/PascalsTriangle.java
@@ -0,0 +1,31 @@
+package Array;
+
+import java.util.Scanner;
+
+public class PascalsTriangle {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = scanner.nextInt();
+
+        for (int i = 0; i < rows; i++) {
+
+            // Print spaces for formatting
+            for (int space = 0; space < rows - i; space++) {
+                System.out.print(" ");
+            }
+
+            int number = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
