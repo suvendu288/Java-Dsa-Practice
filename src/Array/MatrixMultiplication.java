e5791b2e62512cc97b4246a7157d60ce962b4135
suvendu288 <suvendupal288@gmail.com>
2026-03-03 22:11:31 +0530

    Java program for matrix multiplication

---
+++ src/Array/MatrixMultiplication.java
@@ -0,0 +1,42 @@
+package Array;
+
+public class MatrixMultiplication {
+
+    public static void main(String[] args) {
+
+        int[][] matrixA = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+        int[][] matrixB = {
+                {7, 8},
+                {9, 10},
+                {11, 12}
+        };
+
+        int rowsA = matrixA.length;
+        int colsA = matrixA[0].length;
+        int colsB = matrixB[0].length;
+
+        int[][] result = new int[rowsA][colsB];
+
+        // Multiply matrices
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    result[i][j] += matrixA[i][k] * matrixB[k][j];
+                }
+            }
+        }
+
+        // Print result
+        System.out.println("Resultant Matrix:");
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
