1498af335b136e3776f87a4a31186ac5b6760a31
suvendu288 <suvendupal288@gmail.com>
2026-03-06 21:35:17 +0530

    Java solution for Set Matrix Zeroes with input and output

---
+++ src/Array/SetMatrixZeroes.java
@@ -0,0 +1,68 @@
+package Array;
+import java.util.Scanner;
+
+public class SetMatrixZeroes {
+
+    public static void setZeroes(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        boolean[] zeroRows = new boolean[rows];
+        boolean[] zeroCols = new boolean[cols];
+
+        // Step 1: Mark rows and columns that should be zero
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRows[i] = true;
+                    zeroCols[j] = true;
+                }
+            }
+        }
+
+        // Step 2: Set elements to zero
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (zeroRows[i] || zeroCols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int r = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int c = sc.nextInt();
+
+        int[][] matrix = new int[r][c];
+
+        System.out.println("Enter matrix elements:");
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        setZeroes(matrix);
+
+        System.out.println("Matrix after setting zeroes:");
+        printMatrix(matrix);
+
+        sc.close();
+    }
+}
